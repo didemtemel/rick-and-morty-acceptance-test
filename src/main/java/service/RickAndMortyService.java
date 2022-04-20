212a0a0850a6e0bb324f550aec9b662cdf1ba6aa
Didem Temel <44496208+didemtemel@users.noreply.github.com>
2022-04-20 22:27:52 +0300

    Change endpoint urls

--- src/main/java/service/RickAndMortyService.java
+++ src/main/java/service/RickAndMortyService.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 public class RickAndMortyService extends BaseService{
 
-    public ReadableResponse getAllCharacters() { return getRequest("/characters"); }
+    public ReadableResponse getAllCharacters() { return getRequest("/character"); }
 
-    public ReadableResponse getASingleCharacter(Integer id) { return getRequest("/characters/" + id); }
+    public ReadableResponse getASingleCharacter(Integer id) { return getRequest("/character/" + id); }
     
-    public ReadableResponse getMultipleCharacters(List<Integer> ids) { return getRequest("/characters/" + ids); }
+    public ReadableResponse getMultipleCharacters(List<Integer> ids) { return getRequest("/character/" + ids); }
 
-    public ReadableResponse filterCharacters(List<QueryParam> queryParams) { return getRequest("/characters/" + queryParams); }
+    public ReadableResponse filterCharacters(List<QueryParam> queryParams) { return getRequest("/character/" + queryParams); }
 
 
 }
