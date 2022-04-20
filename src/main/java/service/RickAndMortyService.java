635ee29d2dbff6df41cc9ace1a243e2b8a36ece5
Didem Temel <44496208+didemtemel@users.noreply.github.com>
2022-04-20 18:42:28 +0300

    Add endpoints

--- src/main/java/service/RickAndMortyService.java
+++ src/main/java/service/RickAndMortyService.java
@@ -1,6 +1,20 @@
 package service;
 
 
+import util.QueryParam;
+import util.ReadableResponse;
+
+import java.util.List;
+
 public class RickAndMortyService extends BaseService{
+
+    public ReadableResponse getAllCharacters() { return getRequest("/characters"); }
+
+    public ReadableResponse getASingleCharacter(Integer id) { return getRequest("/characters/" + id); }
+    
+    public ReadableResponse getMultipleCharacters(List<Integer> ids) { return getRequest("/characters/" + ids); }
+
+    public ReadableResponse filterCharacters(List<QueryParam> queryParams) { return getRequest("/characters/" + queryParams); }
+
 
 }
